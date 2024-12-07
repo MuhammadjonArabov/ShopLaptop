482aa872641c378af6569d7794f237c1fc753cde
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-12-07 19:57:15 +0500

    add html

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('products/', views.product_list, name='product_list'),
-    # path('products/add/', views.add_product, name='add_product'),
-    # # Boshqa URL yo'llari
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('seller_register/', views.seller_register, name='seller_register'),
+    path('', views.index, name='index'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,51 @@
-from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product, Category
-from .forms import ProductForm
+from .models import User, Seller, Customer
 
+def register(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        phone = request.POST['phone']
+        password = request.POST['password']
+
+        if User.objects.filter(phone=phone).exists():
+            render render(request, 'register.html', {"error": "Bu telefon raqam oldin ro'yxatdan o'tgan"})
+
+        user = User.objects.create_user(full_name=full_name, phone=phone, password=password)
+        Customer.objects.create(user=user)
+        login(request, user)
+        return redirect('index')
+
+    return render(request, 'register.html') 
+
+def login_view(request):
+    if request.method == 'POST':
+        phone = request.POST['phone']
+        password = request.POST['password'] 
+        user = authenticate(request, phone=phone, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {"error": "Telefon yoki parol nato'g'ri!"})
+    return render(request, 'logon.html')
+
+def seller_register(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        phone = request.POST['phone']
+        password = request.POST['password']
+
+        if User.objects.filter(phone=phone).exists():
+            return render('seller_register.html', {"error": "Bu telefon raqami oldin ro'yxatdan o'tgan"})
+
+        user = User.objects.create_user(full_name=full_name, phone=phone, password=parol)
+        Seller.objects.create(user=user)
+        login(request, user)
+        return render('index')
+    return render(request, 'seller_register.html')
+
+@login_required
+def index(request):
+    return render(request, 'index.html', {'user': request.user})                                      
